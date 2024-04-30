04b9c8ea83d914bb5e132962a97a85bc7548577b
PHJUNITY <45454119+PHJUNITY@users.noreply.github.com>
2024-04-30 09:46:41 +0900

    유저가 try한 값 return하고 list에 저장하는 기능 추가

--- src/main/kotlin/BaseBallGame.kt
+++ src/main/kotlin/BaseBallGame.kt
@@ -3,15 +3,17 @@ fun main() {
 }
 
 
+
 fun mainMenu() {
 
+    var userTryList= mutableListOf<Int>()
     var userInput = ""
     println("야구게임에 오신것을 환영합니다! 원하시는 번호를 입력해주세요")
     println("1. 게임 시작하기    2. 게임 기록 보기   3. 종료하기 ")
     userInput = readLine()!!
 
     when (userInput) {
-        "1" -> baseBallGame()
+        "1" -> userTryList.add(baseBallGame()) //baseball 함수의  userTry 리턴값을 리스트로 저장함
         "2" -> println("게임 기록 보는 함수로 이동하자")
         "3" -> System.exit(0) //3번누르면 프로그램 종료
         else -> println("숫자 1,2,3 중에 하나의 값만 입력해주세요")
@@ -19,16 +21,20 @@ fun mainMenu() {
 
 }
 
-fun baseBallGame() {
+fun baseBallGame():Int {
+    var userTry=0
     while(true){
         var computerNumber= mutableListOf(1,2,3)
         var userNumber=""
+
+
         while(true){
             print("정답을 맞추기 위한 서로다른 3자리 숫자를 입력해보세요! : ")
             userNumber = readLine()!!
             if(userNumber.length==3) break
             else println("반드시 3개의 숫자를 입력해야합니다")
         }
+
         var userNumberList=userNumber.map{it.toString().toInt()}
         if(userNumberList[0]==0 || userNumberList.distinct().size != userNumberList.size) println("앞자리가 0이 되서는 안되며 중복된 값이 있어서는 안됩니다. 다시 입력해주세요.")
         else{
@@ -44,9 +50,15 @@ fun baseBallGame() {
                 }
             }
             println("--------- ${countStrike}S ${countBall}B --------------")
-            break
+            userTry+=1 //유저가 시도한 횟수
+            if(countStrike==3) break
+
+
         }
         println(userNumberList)
+        println("-----------------------------------")
+
     }
+    println("총 ${userTry} 번의 시도를 통해 정답을 맞춤!")
+    return userTry
 }
-//
